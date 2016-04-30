9be7f023ec904111104456703eb941d32d490cf8
Jaime Garcia <jaimiho@gmail.com>
2016-04-30 16:00:43 -0400

    Added User model, added nokogiri gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  #set the index page / root url 
+  root 'pages#home'
+
+  get 'pages/about'
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
   resources :city_pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
