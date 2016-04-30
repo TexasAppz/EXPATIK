a73589f898acb32e9851e545bec66566d13b8bfc
Jaime Garcia <jaimiho@gmail.com>
2016-04-30 19:47:04 -0400

    Adding a gem seed_dump

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'seed_dump'
 
 gem 'nokogiri'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+CityPage.create!([
+  {city: "Shanghai", country: "China", continent: "Asia", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Singapore", country: "Singapore", continent: "Asia", article: nil, lat: nil, twitter_feed: nil, book_suggestion: nil, lon: nil},
+  {city: "Cape Town", country: "South Africa", continent: "Africa", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Buenos Aires", country: "Argentina", continent: "South America", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Prague", country: "Czech Republic", continent: "Central Europe", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Dubai", country: "UAE", continent: "Middle East", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Medellin", country: "Colombia", continent: "South America", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Budapest", country: "Hungary", continent: "Central Europe", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Roatan", country: "Honduras", continent: "Central America", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil},
+  {city: "Reykjavik", country: "Iceland", continent: "Europe", article: "", lat: "", twitter_feed: "", book_suggestion: "", lon: nil}
+])
+User.create!([
+  {name: "Justin Bieber", email: "justin@bieber.co"},
+  {name: "Jimmy fallon", email: "jimmy@fallon.co"},
+  {name: "Hale Berry", email: "halle@berry.co"},
+  {name: "Peyton Manning", email: "peyton@broncos.com"},
+  {name: "Mark Zuckerberg", email: "mark@facebook.com"}
+])
